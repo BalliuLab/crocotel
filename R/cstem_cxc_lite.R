997d7ca58317ea408690b685e731a9b4ea1d77f0
lkrocken <lkrocken@g.ucla.edu>
2025-05-21 19:50:41 -0700

    added new lite code

--- R/cstem_cxc_lite.R
+++ R/cstem_cxc_lite.R
@@ -20,15 +20,14 @@
 #' @export
 cstem_gbat_lite = function(regulator_pred_exp_file, target_pred_exp_file, target_exp_files, contexts_vec, method, regulator_gene_name, target_gene_name, outdir, target_cis_pred = T){
   ## get target expression across all contexts
-  regulator_exp_mat = fread(regulator_pred_exp_file, sep = "\t", data.table = F)
+  regulator_exp_mat = fread(regulator_pred_exp_file, sep = "\t", data.table = F, check.names = F)
   
   # get target cis predicted expression across all contexts
-  target_cis_pred_mat = fread(target_pred_exp_file, sep = "\t", data.table = F)
+  target_cis_pred_mat = fread(target_pred_exp_file, sep = "\t", data.table = F, check.names = F)
   
-  this_gene = foreach(context = 1:length(contexts_vec), .combine = 'rbind') %dopar% {
-    context_name = contexts_vec[context]
+  this_gene = bind_rows(lapply(contexts_vec, function(context_name){
     print(paste("Running c-stem lite for gene pair ", regulator_gene_name, " and ", target_gene_name, " in context ", context_name))
-    target_exp_vec = fread(target_exp_files[grepl(context_name, target_exp_files)], sep = "\t", data.table = F)
+    target_exp_vec = fread(target_exp_files[grepl(paste0("/",context_name), target_exp_files)], sep = "\t", data.table = F)
     names(target_exp_vec) = c("id", "target_exp")
     
     regulator_exp_vec = regulator_exp_mat[,c("id", context_name)]
@@ -46,7 +45,7 @@ cstem_gbat_lite = function(regulator_pred_exp_file, target_pred_exp_file, target
       regulator_beta = summary_model$coefficients[2, 1]
       regulator_se = summary_model$coefficients[2,2]
       #target_pvalue <- summary_model$coefficients[3, 4]
-      return(data.frame(regulator_gene_name, target_gene_name, context_name, regulator_beta, regulator_se, regulator_pvalue))#, target_pvalue))
+      df = data.frame(regulator_gene_name, target_gene_name, context_name, regulator_beta, regulator_se, regulator_pvalue)#, target_pvalue))
     } else {
       #trans_model <- lm.fit(cbind(1, regulator_exp_vec), trans_exp_df_vec)
       trans_model2 <- lm(lm_df[,"target_exp"] ~ lm_df[,"regulator_pred"])
@@ -54,18 +53,19 @@ cstem_gbat_lite = function(regulator_pred_exp_file, target_pred_exp_file, target
       regulator_pvalue <- summary_model$coefficients[2, 4]
       regulator_beta = summary_model$coefficients[2, 1]
       regulator_se = summary_model$coefficients[2,2]
-      return(data.frame(regulator_gene_name, target_gene_name, context_name, regulator_beta, regulator_se, regulator_pvalue))
+      df = data.frame(regulator_gene_name, target_gene_name, context_name, regulator_beta, regulator_se, regulator_pvalue)
     }
-  }
+  }))
+  
   if(target_cis_pred){
-    file_prefix = "_cis_cstemlite.txt"
+    file_prefix = ".cis_cstemlite.txt"
     if(method == "GBAT"){
-      file_prefix = "_cis_gbat.txt"
+      file_prefix = ".cis_gbat.txt"
     }
   }else{
-    file_prefix = "_cstemlite.txt"
+    file_prefix = ".cstemlite.txt"
     if(method == "GBAT"){
-      file_prefix = "_gbat.txt"
+      file_prefix = ".gbat.txt"
     }
   }
   fwrite(this_gene, file = paste0(outdir, regulator_gene_name, "_", target_gene_name, file_prefix),  sep = "\t")
