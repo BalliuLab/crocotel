806dd997761e5eb70eb207de3a9065859441730e
lkrocken <lkrocken@g.ucla.edu>
2025-08-23 20:12:54 -0700

    modified concatenation function

--- R/multiple_testing_correction.R
+++ R/multiple_testing_correction.R
@@ -220,7 +220,7 @@ concat_crocotel_lmm_files <- function(directory = ".", context, regress_target_G
     out_file="${prefix}.${file_suffix}"
     tmp_merged="${tmp_outdir}${out_file}"
     echo "finding files to concatenate for context $context"
-    find . -maxdepth 1 -type f -name "${prefix}.*${file_suffix}" -print > "${tmp_outdir}${prefix}_to_concatenate.txt"
+    find . -maxdepth 1 -type f -name "${prefix}.*${file_suffix}" -printf "%p\n" > "${tmp_outdir}${prefix}_to_concatenate.txt"
     xargs awk "FNR==1 && NR!=1 { next } { print }" < "${tmp_outdir}${prefix}_to_concatenate.txt" > $tmp_merged
 
     # Sort by 6th column (p-value) ascending, keeping header
