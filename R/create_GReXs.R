ddc81998022d17a353efe47f82a3cb8f9ba842e8
lkrocken <lkrocken@g.ucla.edu>
2026-03-16 17:33:36 -0700

    updated create GReX to properly residualize target grex

--- R/create_GReXs.R
+++ R/create_GReXs.R
@@ -1,40 +1,33 @@
 
 
-regress_target_GReX = function(gene_name, exp_files, Yhats_tiss, outdir){
-  dir.create(paste0(outdir, "exp_residualized_GReX/"), showWarnings = F)
-  all_residuals = NULL
-  for(file in exp_files){
-    context = gsub(".*/", "", file)
-    context = sub("\\..*", "", context)
-    original_exp = data.frame(fread(file, header = F), check.names = F,stringsAsFactors = F)
-    ids <- original_exp[, 1]
-    expr <- original_exp[, 2]
+regress_target_GReX = function(gene_name, total_exp_mat, crocotel_grex, out_dir, method = "crocotel"){
+  res_dir = paste0(out_dir, "exp_residualized_GReX/")
+  dir.create(res_dir, showWarnings = F)
+  
+  # Make sure IDs match — reorder or subset if needed
+  matching_ids = intersect(total_exp_mat$id, crocotel_grex$id)
+  expr_sub = total_exp_mat %>% filter(id %in% matching_ids) %>% arrange(id) 
+  grex_sub = crocotel_grex %>% filter(id %in% matching_ids) %>% arrange(id)
+  
+  if(sum(dim(expr_sub) == dim(grex_sub))==2){
+    grex_sub_no_id = grex_sub %>% select(-id)
+    expr_sub_no_id = expr_sub %>% select(-id)
+    residual_matrix = matrix(NA, nrow = nrow(grex_sub_no_id), ncol = ncol(grex_sub_no_id),
+                             dimnames = dimnames(grex_sub_no_id))
     
-    cur_grex = Yhats_tiss[[context]]
-    
-    # Make sure IDs match — reorder or subset if needed
-    matching_ids <- intersect(ids, rownames(cur_grex))
-    expr_sub <- expr[ids %in% matching_ids]
-    grex_sub <- cur_grex[matching_ids,]
-    ids_sub <- ids[ids %in% matching_ids]
-    
-    ## get GReX expression of context
-    
-    model <- lm(expr_sub ~ grex_sub, na.action = na.exclude)
-    residuals_vec <- residuals(model)
-    
-    sub_df = data.frame(id = ids_sub, residual = residuals_vec)
-    names(sub_df)[2] = context
-    
-    # Merge into final residuals data frame
-    if (is.null(all_residuals)) {
-      all_residuals = sub_df
-    } else {
-      all_residuals = merge(all_residuals, sub_df, by = "id", all = TRUE)
+    for (i in seq_len(ncol(grex_sub_no_id))) {
+      context = names(grex_sub_no_id)[i]
+      fit = lm(expr_sub[, context] ~ grex_sub[, context], na.action = na.exclude)
+      residual_matrix[, context] = residuals(fit)
     }
+    residual_matrix = data.frame(id = grex_sub$id, residual_matrix)
+    
+    outfile = paste0(res_dir, gene_name, ".", method, ".GReX_residuals.txt")
+    fwrite(residual_matrix, outfile, sep = "\t", quote = F)
+  }else{
+    cat("dimensions of matrices do not align. cannot residualize for target grex")
   }
-  outfile = paste0(outdir, "exp_residualized_GReX/", gene_name, ".crocotel.GReX_residuals.txt")
-  fwrite(all_residuals, outfile, sep = "\t", quote = F)
+  return()
 }
 
 
@@ -50,7 +43,7 @@ regress_target_GReX = function(gene_name, exp_files, Yhats_tiss, outdir){
 #' @param method - Takes values of "crocotel" or "cxc". Default is "crocotel", if "cxc" then will not run decomposition and will build GReXs in a context by context manner.
 #' @return writes out a file of predicted expression across individuals and contexts 
 #' @export
-create_GReXs = function(gene_name, out_dir, genotype_file = NULL, exp_files = NULL, context_thresh = 2, alpha = 0.5, num_folds = 10, impute = F, run_cxc = F, save_decomposition = F){
+create_GReXs = function(gene_name, out_dir, genotype_file = NULL, exp_files = NULL, context_thresh = 2, alpha = 0.5, num_folds = 10, impute = F, run_cxc = F, save_decomposition = F, regress_tGReX = T){
   seed = 9000
   set.seed(seed)
   GReX_outdir = paste0(out_dir, "GReXs/")
@@ -111,6 +104,10 @@ create_GReXs = function(gene_name, out_dir, genotype_file = NULL, exp_files = NU
   message("Running crocotel")
   crossval_output = crossval_helper_parallel(total_exp_mat, decomp_exp_mat, X, GReX_outdir, gene_name, FALSE, num_folds, alpha)
   
+  if(regress_tGReX){
+    regress_target_GReX(gene_name, total_exp_mat, crossval_output$full, out_dir, method = "crocotel")
+  }
+  
   message("Finished computing GReXs and evalutation metrics")
   
   #### impute using cxc if flag is set
@@ -122,6 +119,12 @@ create_GReXs = function(gene_name, out_dir, genotype_file = NULL, exp_files = NU
     # write out imputed results
     method = "crocotel_imputed"
     fwrite(crocotel_grex,  file = paste0(GReX_outdir, gene_name,".", method, ".GReX_predictors.txt"), sep = "\t")
+    if(regress_tGReX){
+      regress_target_GReX(gene_name, total_exp_mat, crocotel_grex, out_dir, method) 
+    }
   }
+  
+  
+  
   
 }
